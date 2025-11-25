a0ce823baf1157bd788ebdb67fb27168d2ba598f
Shakuntala Mitra <smitra@insilica.co>
2025-11-25 12:43:26 -0500

    updated the lib base to export from the openapi component: AuthenticatedClient and Client classes, api module to access endpoints, all models, all error types, all types

--- bases/sysrev_client/lib/__init__.py
+++ bases/sysrev_client/lib/__init__.py
@@ -1,3 +1,14 @@
-from sysrev_client.api import *
+from sysrev_client.openapi.gen.client import AuthenticatedClient, Client
+from sysrev_client.openapi.gen.errors import *
+from sysrev_client.openapi.gen.models import *
+from sysrev_client.openapi.gen.types import *
 
-__all__ = [name for name in globals() if not name.startswith("_")]
+from sysrev_client.openapi.gen import api
+
+__all__ = [
+    "AuthenticatedClient",
+    "Client",
+    "api",
+]
+
+__all__.extend([name for name in dir() if not name.startswith("_") and name not in __all__])
